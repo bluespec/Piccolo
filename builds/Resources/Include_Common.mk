c5cfd428fae81b673f0d6cd44e20cb0aa52b58e8
Alexandre Joannou <aj443@cam.ac.uk>
2019-03-26 17:13:46 +0000

    AXI4-unify: update makefiles include to point at the new AXI4 library

--- builds/Resources/Include_Common.mk
+++ builds/Resources/Include_Common.mk
@@ -36,9 +36,15 @@ all: compile  simulator
 
 CORE_DIRS = $(REPO)/src_Core/CPU:$(REPO)/src_Core/ISA:$(REPO)/src_Core/RegFiles:$(REPO)/src_Core/Core:$(REPO)/src_Core/Near_Mem_VM:$(REPO)/src_Core/PLIC:$(REPO)/src_Core/Near_Mem_IO:$(REPO)/src_Core/Debug_Module:$(REPO)/src_Core/BSV_Additional_Libs
 
-TESTBENCH_DIRS  = $(REPO)/src_Testbench/Top:$(REPO)/src_Testbench/SoC:$(REPO)/src_Testbench/Fabrics/AXI4
+TESTBENCH_DIRS  = $(REPO)/src_Testbench/Top:$(REPO)/src_Testbench/SoC
 
-BSC_PATH = -p $(CORE_DIRS):$(TESTBENCH_DIRS):+
+# XXX
+# Specify the path to the BlueStuff library (https://github.com/CTSRD-CHERI/BlueStuff)
+# What is the best way to distribute this code?
+# XXX
+AXI_DIRS = $(REPO)/../BlueStuff/AXI:$(REPO)/../BlueStuff/BlueBasics:$(REPO)/../BlueStuff/
+
+BSC_PATH = -p $(CORE_DIRS):$(TESTBENCH_DIRS):$(AXI_DIRS):+
 
 # ----------------
 # Top-level file and module
@@ -49,8 +55,15 @@ TOPMODULE ?= mkTop_HW_Side
 # ================================================================
 # bsc compilation flags
 
+# XXX
+# Using '-no-show-timestamps' with
+# Bluespec Compiler, version 2017.07.A (build 1da80f1, 2017-07-21)
+# results in
+# Error: Command line: (S0008)
+#   Unrecognized flag: -no-show-timestamps
+# XXX
 BSC_COMPILATION_FLAGS += \
-	-keep-fires -aggressive-conditions -no-warn-action-shadowing -no-show-timestamps -check-assert \
+	-keep-fires -aggressive-conditions -no-warn-action-shadowing -check-assert \
 	-suppress-warnings G0020    \
 	+RTS -K128M -RTS  -show-range-conflict
 
